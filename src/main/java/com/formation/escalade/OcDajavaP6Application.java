b5f8ddbf02e623123d101bfe01677216799c4ae4
Senpothia <kintelalpz@gmail.com>
2020-01-23 05:50:21 +0100

    init

---
+++ src/main/java/com/formation/escalade/OcDajavaP6Application.java
@@ -0,0 +1,13 @@
+package com.formation.escalade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcDajavaP6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcDajavaP6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/escalade/OcDajavaP6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.escalade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcDajavaP6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
