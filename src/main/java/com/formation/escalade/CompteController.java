02efea7447af6929363f4d6845d51d040e89cf64
Senpothia <kintelalpz@gmail.com>
2020-01-26 22:45:48 +0100

    Corrections espace.html

--- src/main/java/com/formation/escalade/CompteController.java
+++ src/main/java/com/formation/escalade/CompteController.java
@@ -1,13 +1,19 @@
 package com.formation.escalade;
 
+import java.lang.module.FindException;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.formation.escalade.model.Utilisateur;
 import com.formation.escalade.repository.IUtilisateur;
+import com.sun.xml.bind.v2.model.core.ID;
 
 @Controller
 public class CompteController {
@@ -42,7 +48,7 @@ public class CompteController {
 		return "modificationCompte";
 	}
 	
-	@PostMapping("/ModificationCompte")
+	@PostMapping("/modificationCompte")
 	public String compteModification(@ModelAttribute Utilisateur utilisateur, Model model) {
 		model.addAttribute("utilisateur", utilisateur);
 		System.out.println(utilisateur.toString());
@@ -54,16 +60,27 @@ public class CompteController {
 	}
 	
 	@GetMapping("/espace")
-	public String espace(Utilisateur utilisateur) {
-		
+	public String espace(Model model) {
+		model.addAttribute("utilisateur", new Utilisateur(100, "Hugo", "Victor", 75, "mail@hhg", "passvictor",false));
 		return "espace";
 	}
 	
 	@PostMapping("/espace")
 	public String espaceSubmit(@ModelAttribute Utilisateur utilisateur, Model model) {
-		model.addAttribute("utilisateur", utilisateur);
+		model.addAttribute("utilisateur", new Utilisateur(100, "Hugo", "Victor", 75, "mail@hhg", "passvictor",false));
 		System.out.println(utilisateur.toString());
 		return "espace";
 	}
+	
+	@GetMapping("/suppressionCompte/{id}") 
+		
+		public void supprimerCompte(@PathVariable int id) {
+		 //utilisateurRepo.deleteInBatch(utilisateurRepo.findById(id));
+			
+		       System.out.println("Utilisateur id: " + id +"Supprimé");
+		   }
+   
+		
+	}
 
-}
+
--- src/main/java/com/formation/escalade/repository/IUtilisateur.java
+++ src/main/java/com/formation/escalade/repository/IUtilisateur.java
@@ -1,11 +1,18 @@
 package com.formation.escalade.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.formation.escalade.model.Utilisateur;
+import com.sun.xml.bind.v2.model.core.ID;
 
-@Component
+@Repository
 public interface IUtilisateur extends JpaRepository <Utilisateur, Integer>{
 
+	 Utilisateur findById(int id);
+	
+	
 }
