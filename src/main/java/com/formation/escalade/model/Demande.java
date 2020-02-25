ddb30cd0b873b8368e706f86e28d6802971de8d8
Michel Lopez <kintela@hotmail.fr>
2020-02-25 11:27:02 +0100

    Modification classe demande

--- src/main/java/com/formation/escalade/model/Demande.java
+++ src/main/java/com/formation/escalade/model/Demande.java
@@ -11,39 +11,65 @@ public class Demande {
 	private Integer id;
 	private Integer id_topo;
 	private Integer id_utilisateur;
-	private Integer idTopo;
-	private Integer idUtilisateur;
-	
-	
+	//private Integer idTopo;
+	//private Integer idUtilisateur;
 	
 	public Demande() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Demande(Integer id, Integer id_topo, Integer id_utilisateur) {
+
+	public Demande(Integer id, Integer id_topo, Integer id_utilisateur, Integer idTopo, Integer idUtilisateur) {
 		super();
 		this.id = id;
 		this.id_topo = id_topo;
 		this.id_utilisateur = id_utilisateur;
+		//this.idTopo = idTopo;
+		//this.idUtilisateur = idUtilisateur;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getId_topo() {
 		return id_topo;
 	}
+
 	public void setId_topo(Integer id_topo) {
 		this.id_topo = id_topo;
 	}
+
 	public Integer getId_utilisateur() {
 		return id_utilisateur;
 	}
+
 	public void setId_utilisateur(Integer id_utilisateur) {
 		this.id_utilisateur = id_utilisateur;
 	}
+/**
+	public Integer getIdTopo() {
+		return idTopo;
+	}
+
+	public void setIdTopo(Integer idTopo) {
+		this.idTopo = idTopo;
+	}
+
+	public Integer getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(Integer idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+	*/
+	
+	
 	
 	
 }
