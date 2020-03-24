8f222d0b7f0069bdf7c21ce8110760125bf24896
Senpothia <kintelalpz@gmail.com>
2020-03-24 09:44:21 +0100

    Ajout controller Recherche

---
+++ src/main/java/com/formation/escalade/RechercheController.java
@@ -0,0 +1,39 @@
+package com.formation.escalade;
+
+import org.springframework.stereotype.Controller;
+
+import com.formation.escalade.repository.ICommentaire;
+import com.formation.escalade.repository.ILongueur;
+import com.formation.escalade.repository.ISecteur;
+import com.formation.escalade.repository.ISite;
+import com.formation.escalade.repository.ITopo;
+import com.formation.escalade.repository.IUtilisateur;
+import com.formation.escalade.repository.IVoie;
+
+@Controller
+public class RechercheController {
+	
+	private final ISite siteRepo;
+	private final ISecteur secteurRepo;
+	private final IVoie voieRepo;
+	private final ILongueur longueurRepo;
+	private final ICommentaire commentaireRepo;
+	private final IUtilisateur utilisateurRepo;
+	private final ITopo topoRepo;
+	
+	public RechercheController(ISite siteRepo, ISecteur secteurRepo, IVoie voieRepo, ILongueur longueurRepo,
+			ICommentaire commentaireRepo, IUtilisateur utilisateurRepo, ITopo topoRepo) {
+		super();
+		this.siteRepo = siteRepo;
+		this.secteurRepo = secteurRepo;
+		this.voieRepo = voieRepo;
+		this.longueurRepo = longueurRepo;
+		this.commentaireRepo = commentaireRepo;
+		this.utilisateurRepo = utilisateurRepo;
+		this.topoRepo = topoRepo;
+	}
+	
+	
+	
+
+}
