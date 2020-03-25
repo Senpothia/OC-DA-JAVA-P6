0997b9eb71905be51343389de299548cc2d8625b
Senpothia <kintelalpz@gmail.com>
2020-03-25 18:14:51 +0100

    Implémentation suppression secteur

--- src/main/java/com/formation/escalade/SecteurController.java
+++ src/main/java/com/formation/escalade/SecteurController.java
@@ -1,5 +1,9 @@
 package com.formation.escalade;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -11,11 +15,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.formation.escalade.model.FormSite;
+import com.formation.escalade.model.Longueur;
+import com.formation.escalade.model.Secteur;
 import com.formation.escalade.model.Site;
+import com.formation.escalade.model.Voie;
 import com.formation.escalade.repository.ICommentaire;
 import com.formation.escalade.repository.ILongueur;
 import com.formation.escalade.repository.ISecteur;
 import com.formation.escalade.repository.ISite;
+import com.formation.escalade.repository.IUtilisateur;
 import com.formation.escalade.repository.IVoie;
 import com.formation.escalade.service.SecteurService;
 import com.formation.escalade.service.SiteService;
@@ -31,15 +39,17 @@ public class SecteurController {
 	private final IVoie voieRepo;
 	private final ILongueur longueurRepo;
 	private final ICommentaire commentaireRepo;
+	private final IUtilisateur utilisateurRepo;
 
 	public SecteurController(ISite siteRepo, ISecteur secteurRepo, IVoie voieRepo, ILongueur longueurRepo,
-			ICommentaire commentaireRepo) {
+			ICommentaire commentaireRepo, IUtilisateur utilisateurRepo) {
 
 		this.siteRepo = siteRepo;
 		this.secteurRepo = secteurRepo;
 		this.voieRepo = voieRepo;
 		this.longueurRepo = longueurRepo;
 		this.commentaireRepo = commentaireRepo;
+		this.utilisateurRepo = utilisateurRepo;
 	}
 
 	@GetMapping("/creation_secteur")
@@ -85,8 +95,69 @@ public class SecteurController {
 			model.addAttribute("erreur", true);
 			return "creation_secteur";
 		}
-
+	}
+	
+	@GetMapping("/supprimer/site/{id}/secteur")
+	public String choixSecteur_del(@PathVariable("id") Integer id, Model model
+			, HttpSession session, Principal principal
+			, HttpServletRequest request) {
 		
+		try {
+
+			String email = request.getUserPrincipal().getName();
+			System.out.println("email récupéré: " + email);
+			model.addAttribute("utilisateur", utilisateurRepo.findByEmail(email));
+			model.addAttribute("authentification", true);
+
+		} catch (NullPointerException e) {
+
+			System.out.println("email récupéré: aucun!!!");
+			model.addAttribute("authentification", false);
+		}
+		
+		Site site = siteRepo.getOne(id);
+		List<Secteur> secteurs = site.getSecteurs();
+		model.addAttribute("site", site);
+		
+		return "choisirsecteur_delete";
+	}
+	
+	@PostMapping("/supprimer/site/secteur/{id}")
+	public String supprimerSecteur(@PathVariable("id") Integer id, Model model
+			, HttpSession session, Principal principal
+			, HttpServletRequest request, String nomSecteur) {
+		
+		try {
+
+			String email = request.getUserPrincipal().getName();
+			System.out.println("email récupéré: " + email);
+			model.addAttribute("utilisateur", utilisateurRepo.findByEmail(email));
+			model.addAttribute("authentification", true);
+
+		} catch (NullPointerException e) {
+
+			System.out.println("email récupéré: aucun!!!");
+			model.addAttribute("authentification", false);
+		}
+		
+		
+		System.out.println("nom secteur récupéré à supprimer: " + nomSecteur);
+		
+		Site site = siteRepo.getOne(id);
+		Secteur secteur = secteurRepo.findByNom(nomSecteur);
+		List<Voie> voies = voieRepo.findBySecteur(secteur);
+		List<Longueur> longueurs = new ArrayList<Longueur>();
+		
+		for (Voie voie: voies ) {
+			
+			longueurs.addAll(voie.getLongueurs());
+			longueurRepo.deleteAll(longueurs);
+		}
+		
+		voieRepo.deleteAll(voies);
+		secteurRepo.delete(secteur);
+		
+		return "espace";
 	}
 
 }
