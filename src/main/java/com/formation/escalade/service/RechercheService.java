6f7f5b623e49ab83c701371fb21cc9f92a0b9e9c
Senpothia <kintelalpz@gmail.com>
2020-03-27 11:05:56 +0100

    Modification implémentation moteur de recherche

--- src/main/java/com/formation/escalade/service/RechercheService.java
+++ src/main/java/com/formation/escalade/service/RechercheService.java
@@ -53,7 +53,7 @@ public class RechercheService {
 		this.commentaireRepo = commentaireRepo;
 		this.utilisateurRepo = utilisateurRepo;
 	}
-	
+
 	public List<Utilisateur> rechercheCreateur(Set<Site> sites) {
 
 		List<Utilisateur> createurs = new ArrayList<Utilisateur>();
@@ -72,8 +72,11 @@ public class RechercheService {
 	public Set<Site> recherche(FormSearch formSearch) {
 
 		Set<Site> sites = new LinkedHashSet<>(new ArrayList<Site>()); // liste à transmettre page html
-		Set<Site> sites_copie = new LinkedHashSet<>(new ArrayList<Site>()); 
-		Set<Site> sites_aux = new LinkedHashSet<>(new ArrayList<Site>()); 
+		Set<Site> sites_copie = new LinkedHashSet<>(new ArrayList<Site>());
+		Set<Site> sites_aux = new LinkedHashSet<>(new ArrayList<Site>());
+		Set<Site> sites_parNom = new LinkedHashSet<>(new ArrayList<Site>()); 
+		Set<Site> sites_parCreateur = new LinkedHashSet<>(new ArrayList<Site>()); 
+		/*
 		try {
 			// recherche par nom de site
 			Site site = siteRepo.findByNomIgnoreCase(formSearch.getNom());
@@ -113,10 +116,11 @@ public class RechercheService {
 		}
 
 		// Recherche par nom de createur
-		
+
 		try {
 
-			List<Utilisateur> createurs = utilisateurRepo.findByNomOrPrenomIgnoreCase(formSearch.getCreateur(),formSearch.getCreateur());
+			List<Utilisateur> createurs = utilisateurRepo.findByNomOrPrenomIgnoreCase(formSearch.getCreateur(),
+					formSearch.getCreateur());
 			for (Utilisateur createur : createurs) {
 
 				Integer id = createur.getId();
@@ -126,68 +130,135 @@ public class RechercheService {
 
 		} catch (NullPointerException e) {
 		}
-		
+
 		// Tri selon le département
-		
-		if (formSearch.getDepartement() != 0){
-			
+
+		if (formSearch.getDepartement() != 0) {
+
 			if (sites.isEmpty()) {
-				
+
 				System.out.println("liste sites vide, recherche par département");
 				sites.addAll(siteRepo.findByDepartement(formSearch.getDepartement()));
-				
+
 			} else {
-				
+
 				System.out.println("liste sites non vide, recherche par département");
 				sites_copie.addAll(sites);
-				for (Site site: sites_copie) {
-					
-					Integer departement = site.getDepartement();
-					if (departement !=  formSearch.getDepartement()) {
-						
-						sites.remove(site);
+
+				if (formSearch.getNom() != "" || formSearch.getCreateur() != "" || formSearch.getLocalisation() != "") {
+
+					for (Site site : sites_copie) {
+
+						Integer departement = site.getDepartement();
+						if (departement != formSearch.getDepartement()) {
+
+							sites.remove(site);
+						}
+
+						sites_aux.addAll(siteRepo.findByDepartement(formSearch.getDepartement()));
+						sites.addAll(sites_aux);
 					}
-					
-					sites_aux.addAll(siteRepo.findByDepartement(formSearch.getDepartement()));
-					sites.addAll(sites_aux);
+
 				}
-				
+
 			}
-			
+
 		}
-		
-		
+
 		// Tri selon officiel
 		if (formSearch.getNom().equals("")) {
-			
+
 			String typeChoisi = formSearch.getType();
 			System.out.println("Type choisi: " + typeChoisi);
 			System.out.println("Test officiel: " + typeChoisi.equals("Officiel"));
 			sites_copie.addAll(sites);
 			if (!typeChoisi.equals("Tous")) {
-			for (Site site: sites_copie) {
-				
-				Boolean status = site.isOfficiel();
+				for (Site site : sites_copie) {
 
-				if (typeChoisi.equals("Officiel") && !status) {
-					
-					sites.remove(site);
+					Boolean status = site.isOfficiel();
+
+					if (typeChoisi.equals("Officiel") && !status) {
+
+						sites.remove(site);
+					}
+
+					if (typeChoisi.equals("Autres") && status) {
+
+						sites.remove(site);
+					}
+
 				}
-				
-				if (typeChoisi.equals("Autres") && status) {
-					
-					sites.remove(site);
-				}
-					
-			}
-			
+
 			}
 		}
+		*/
+		
+		// Recherche par nom de site, secteur, voie, longueur
+		
+		if (formSearch.getNom() != "") {
+			
+			Site site = siteRepo.findByNomIgnoreCase(formSearch.getNom());
+			if (site != null) {
+			sites_parNom.add(site);
+			
+			}
+			
+			Secteur secteur = secteurRepo.findByNomIgnoreCase(formSearch.getNom());
+			if (secteur !=null) {
+				
+			Site site_nom_secteur = secteur.getSite();
+			sites_parNom.add(site_nom_secteur);
+			}
+			
+			Voie voie = voieRepo.findByNomIgnoreCase(formSearch.getNom());
+			if (voie != null) {
+				
+				Secteur secteur_voie = voie.getSecteur();
+				Site site_voie = secteur_voie.getSite();
+				sites_parNom.add(site_voie);
+			}
+			
+			Longueur longueur = longueurRepo.findByNomIgnoreCase(formSearch.getNom());
+			if (longueur != null) {
+			Voie voie_longueur = longueur.getVoie();
+			Secteur secteur_longueur = voie_longueur.getSecteur();
+			Site site_longueur = secteur_longueur.getSite();
+			sites_parNom.add(site_longueur);
+			}
+		
+		}
+		
+		// Recherche par nom de createur
+		
+		if (formSearch.getCreateur() != "") {
+			
+			try {
+
+				List<Utilisateur> createurs = utilisateurRepo.findByNomOrPrenomIgnoreCase(formSearch.getCreateur(),
+						formSearch.getCreateur());
+				for (Utilisateur createur : createurs) {
+
+					Integer id = createur.getId();
+					List<Site> sites1 = siteRepo.findByCreateur(id);
+					sites_parCreateur.addAll(sites1);
+				}
+
+			} catch (NullPointerException e) {
+			}
+
+			
+		}
+		
+		// Recherche par département
 		
 		
+		
+		
+		
+		
+		sites.addAll(sites_parCreateur);
+		sites.addAll(sites_parNom);
 		return sites;
 	}
-
-	
 
 }
